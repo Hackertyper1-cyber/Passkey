a959178650e7f1a5e57c4cbd3d9e1886e3147cd4
Hackertyper1-cyber <kemuel@sevenkidscodefoundation.org>
2026-06-15 12:40:54 +0100

    first draft of PassKey

---
+++ script.js
@@ -0,0 +1,72 @@
+const password = document.getElementById("password");
+const fill = document.getElementById("fill");
+const message = document.getElementById("message");
+const toggleBtn = document.getElementById("toggleBtn");
+
+const lengthText = document.getElementById("length");
+const uppercaseText = document.getElementById("uppercase");
+const numberText = document.getElementById("number");
+const symbolText = document.getElementById("symbol");
+
+/* Show/Hide Password */
+toggleBtn.addEventListener("click", () => {
+  if (password.type === "password") {
+    password.type = "text";
+    toggleBtn.innerHTML =
+      '<i class="fa-solid fa-eye-slash"></i>';
+  } else {
+    password.type = "password";
+    toggleBtn.innerHTML =
+      '<i class="fa-solid fa-eye"></i>';
+  }
+});
+
+/* Strength Checker */
+password.addEventListener("input", () => {
+
+  const value = password.value;
+
+  const hasLength = value.length >= 8;
+  const hasUppercase = /[A-Z]/.test(value);
+  const hasNumber = /[0-9]/.test(value);
+  const hasSymbol = /[^A-Za-z0-9]/.test(value);
+
+  let score = 0;
+
+  if (hasLength) score++;
+  if (hasUppercase) score++;
+  if (hasNumber) score++;
+  if (hasSymbol) score++;
+
+  lengthText.textContent =
+    `${hasLength ? "✅" : "❌"} At least 8 characters`;
+
+  uppercaseText.textContent =
+    `${hasUppercase ? "✅" : "❌"} One uppercase letter`;
+
+  numberText.textContent =
+    `${hasNumber ? "✅" : "❌"} One number`;
+
+  symbolText.textContent =
+    `${hasSymbol ? "✅" : "❌"} One special character`;
+
+  if (value.length === 0) {
+    fill.style.width = "0%";
+    message.textContent = "Password Strength";
+  }
+  else if (score <= 1) {
+    fill.style.width = "33%";
+    fill.style.background = "#ff4d4d";
+    message.textContent = "Weak Password";
+  }
+  else if (score <= 3) {
+    fill.style.width = "66%";
+    fill.style.background = "#ffc107";
+    message.textContent = "Medium Password";
+  }
+  else {
+    fill.style.width = "100%";
+    fill.style.background = "#28c76f";
+    message.textContent = "Strong Password";
+  }
+});
